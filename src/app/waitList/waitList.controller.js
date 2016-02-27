fb7037acbc5af44822e7d943561ed32d42af0867
Cynrick Chiu <cyn.2k5@outlook.com>
2016-02-27 15:50:31 -0800

    Add Party function constructor

--- src/app/waitList/waitList.controller.js
+++ src/app/waitList/waitList.controller.js
@@ -11,12 +11,22 @@
     var vm = this;
 
     var fireParties = new Firebase('https://e-waiting-list.firebaseio.com/parties');
-    vm.parties = $firebaseArray(fireParties);
 
+    vm.parties = $firebaseArray(fireParties);
+    vm.newParty = new Party();
     vm.addParty = addParty;
 
+    function Party() {
+      this.name = '';
+      this.phone = '';
+      this.size = '';
+      this.done = false;
+      this.notified = false;
+    }
+
     function addParty() {
-      vm.parties.$add('another');
+      vm.parties.$add(vm.newParty);
+      vm.newParty = new Party();
     }
   }
 
