d954ca0d8fd0aa73d7adcc51a087d019add7b709
Cynrick Chiu <cyn.2k5@outlook.com>
2016-02-27 20:44:31 -0800

    Inject firebaseAuth dependency, add function to register user

--- src/app/auth/auth.controller.js
+++ src/app/auth/auth.controller.js
@@ -5,8 +5,30 @@
     .module('app.auth')
     .controller('AuthController', AuthController);
 
-  function AuthController() {
-    
+  AuthController.$inject = ['$firebaseAuth'];
+
+  function AuthController($firebaseAuth) {
+    var vm = this;
+
+    var firebaseReference = new Firebase('https://e-waiting-list.firebaseio.com/');
+    var firebaseAuthObject = $firebaseAuth(firebaseReference);
+
+    vm.user = {
+      email: '',
+      password: ''
+    };
+
+    vm.register = register;
+
+    function register(user) {
+      return firebaseAuthObject.$createUser(user)
+        .then(function(user) {
+          console.log(user);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
   }
 
 })();
