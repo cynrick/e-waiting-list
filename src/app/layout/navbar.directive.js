bd2b8b8836dbdab06c5087a206f45a83b3935a77
Cynrick Chiu <cyn.2k5@outlook.com>
2016-02-28 17:16:32 -0800

    Add layout module

---
+++ src/app/layout/navbar.directive.js
@@ -0,0 +1,31 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app.layout')
+    .directive('customNavbar', customNavbar);
+
+  function customNavbar() {
+    return {
+      templateUrl: 'app/layout/navbar.html',
+      restrict: 'E',
+      controller: NavbarController,
+      controllerAs: 'vm'
+    };
+  }
+
+  NavbarController.$inject = ['$location', 'authService'];
+
+  function NavbarController($location, authService) {
+    var vm = this;
+
+    vm.isLoggedIn = authService.isLoggedIn;
+    vm.logout = logout;
+
+    function logout() {
+      authService.logout();
+      $location.path('/');
+    }
+  }
+
+})();
