222e8d12759dfd49a8142560a7e5d36514688647
Cynrick Chiu <cyn.2k5@outlook.com>
2016-02-28 21:33:10 -0800

    Update customPartyForm directive

--- src/app/waitList/directives/partyForm.directive.js
+++ src/app/waitList/directives/partyForm.directive.js
@@ -8,8 +8,28 @@
   function customPartyForm() {
     return {
       templateUrl: 'app/waitList/directives/partyForm.html',
-      restrict: 'E'
+      restrict: 'E',
+      scope: {
+        parties: '='
+      },
+      controller: PartyFormController,
+      controllerAs: 'vm',
+      bindToController: true
     };
+  }
+
+  PartyFormController.$inject = ['partyService'];
+
+  function PartyFormController(partyService) {
+    var vm = this;
+
+    vm.newParty = new partyService.Party();
+    vm.addParty = addParty;
+
+    function addParty() {
+      vm.parties.$add(vm.newParty);
+      vm.newParty = new partyService.Party();
+    }
   }
 
 })();
